fe186e2c7c73e07f26831a717db79543f9a2dab6
«Mikhail» <kopfgolova@gmail.com>
2022-06-03 18:17:39 +0300

    add Task 1

--- Task 1/Program.cs
+++ Task 1/Program.cs
@@ -3,4 +3,33 @@
 упорядочит по убыванию элементы каждой строки двумерного массива.
 */
 
-int [,] FillArray(int)
+int GetDimension(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+int[,] FillArray(int firstDimension, int secondDimension)
+{
+    int[,] array = new int[firstDimension, secondDimension];
+    Random rnd = new Random();
+    for (int i = 0; i < firstDimension; i++)
+        for (int j = 0; j < secondDimension; j++)
+            array[i, j] = rnd.Next(-99, 100);
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j]}  ");
+        Console.WriteLine();
+        }
+}
+
+int m = GetDimension("Введите количество строк массива: ");
+int n = GetDimension("Введите количество столбцов массива: ");
+int [,] arr = FillArray(m, n);
+PrintArray(arr);
